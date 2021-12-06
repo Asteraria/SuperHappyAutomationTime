99362bda3074f8afd2a256b854a1e87b7327f7b5
strublen8556 <strublen8556@my.uwstout.edu>
2021-12-06 17:41:19 +0000

    changed ip addressing, removed nonexistent router 3

--- netmiko_int.py
+++ netmiko_int.py
@@ -18,26 +18,9 @@ print('Closing connection')
 connection.disconnect()
 print('#'*40)
 
-router2 = {'device_type':'cisco_ios','host':'192.168.56.104','username':'cisco','password':'cisco123!','port':22,'secret':'cisco123!','verbose':'True'}
+router2 = {'device_type':'cisco_ios','host':'192.168.56.107','username':'cisco','password':'cisco123!','port':22,'secret':'cisco123!','verbose':'True'}
 
 connection = ConnectHandler(**router2)
-prompt = connection.find_prompt()
-if '>' in prompt:
-    connection.enable()
-
-output = connection.send_command('show ip int brief')
-print(output)
-
-connection.config_mode()
-output = connection.send_command('username cisco secret cisco123!')
-connection.exit_config_mode() 
-print('Closing connection')
-connection.disconnect()
-print('#'*40)
-
-router3 = {'device_type':'cisco_ios','host':'192.168.56.106','username':'cisco','password':'cisco123!','port':22,'secret':'cisco123!','verbose':'True'}
-
-connection = ConnectHandler(**router3)
 prompt = connection.find_prompt()
 if '>' in prompt:
     connection.enable()
